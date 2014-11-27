1ccb441348116529180e398a740741b2f250f20a
necrophcodr <necrophcodr@protonmail.ch>
2014-11-27 11:56:56 +0100

    Finalize index page.

--- index.php
+++ index.php
@@ -11,10 +11,18 @@ include_once( "lib/core.php" );
 		<div id="wrapper">
 			<?php html_dashboard(); ?>
 			<div id="content">
-				<?php html_logo(); ?>
+				<?php html_logo(); html_sidebar(); ?>
+				<h2>Velkommen til Middelfart Lokal TV Projekt Databasen</h2>
+				<p>
+				Her kan du oprette og se projekter.
+				<br />
+				Klik blot <a href="project.php?edit">Opret Projekt</a>, og du er igang.
+				</p>
+				<p>
+				Du kan gennemse alle projekter ved at klikke på <a href="project.php">Gennemse  Projekter</a> øverst, og ønsker du at søge, kan du blot skrive i søgefeltet og trykke <code>ENTER</code>, for at søge på projektnumre, beskrivelser, titler, og alt andet information.
+				</p>
 			</div>
 		</div>
 		<?php html_footer(); ?>
-		<script src="js/zepto.min.js"></script>
 	</body>
 </html>
