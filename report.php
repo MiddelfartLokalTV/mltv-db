de2c0b60120fbf50dfb00e6e7d1e06e9d01c8690
necrophcodr <necrophcodr@protonmail.ch>
2014-11-29 12:50:15 +0100

    Begin implementation of report.php.

--- report.php
+++ report.php
@@ -12,8 +12,27 @@ include_once( "lib/core.php" );
 			<?php html_dashboard(); ?>
 			<div id="content">
 				<?php html_logo(); ?>
+
+				<? if( isset( $_POST["startdate"] ) AND isset( $_POST["enddate"] ) ) { ?>
+				<? } else { ?>
+					<div id="sidebar">
+						<a href="#" onClick="reportCreate();">Opret Rapport</a>
+					</div>
+					<form action="report.php" method="post">
+						<label>Start Dato</label>
+							<input type="date" name="startdate" />
+						<label>Slut Dato</label>
+							<input type="date" name="enddate" />
+						<input type="submit" name="submit" style="display:none" />
+					</form>
+				<? } ?>
 			</div>
 		</div>
 		<?php html_footer(); ?>
+<script>
+function reportCreate() {
+				document.getElementByTagName("form").submit.click();
+			}
+		</script>
 	</body>
 </html>
