dcf891833b33f51712e6d1b9b7be98d0e2a1042a
necrophcodr <necrophcodr@protonmail.ch>
2015-03-24 21:54:07 +0100

    Fix footer copyright date.

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -3,7 +3,7 @@
 		<div id="footer">
 			<p>
 System og design af <a href="http://necrophcodr.me">Steffen Rytter Postas</a>
-Copyright &copy; 2014 <a href="http://www.middelfart.tv">Middelfart Lokal TV</a>
+Copyright &copy; 2015 <a href="http://www.middelfart.tv">Middelfart Lokal TV</a>
 			</p>
 		</div>
 		<script>
